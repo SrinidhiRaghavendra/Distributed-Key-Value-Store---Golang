8ac4d6f43848b5b6b1eb9280011fa8681919e321
Srinidhi Raghavendra <sraghav2@cs.binghamton.edu>
2020-11-29 22:21:21 -0500

    Inital commit. Thrift file written. Source code from thrift generated

---
+++ key-value-store.thrift
@@ -0,0 +1,38 @@
+exception SystemException {
+  1: optional string message
+}
+
+struct KVData {
+  1: string value;
+  2: string timestamp;
+  3: i32 key;
+}
+
+struct Node {
+  1: string id;
+  2: string ip;
+  3: i32 port;
+}
+
+enum ConsistencyLevel {
+  ONE,
+  QUORUM
+}
+
+service Replica {
+  string get(1: i32 key, 2: ConsistencyLevel cLevel)
+    throws (1: SystemException systemException),
+  
+  void put(1: i32 key, 2: string value, 3: ConsistencyLevel cLevel)
+    throws (1: SystemException systemException),
+  
+  KVData getDataFromNode(1: i32 key) 
+    throws (1: SystemException systemException),
+
+  void putDataInNode(1: KVData data) 
+    throws (1: SystemException systemException),
+
+  list<KVData> getHints(1: Node node) 
+    throws (1: SystemException systemException),
+}
+
